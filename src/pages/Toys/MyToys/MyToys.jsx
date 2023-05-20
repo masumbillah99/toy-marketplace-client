27ee052b7013807a12fae1c274a4cc30e64bc80b
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-05-20 21:33:22 +0600

    delete single toy

--- src/pages/Toys/MyToys/MyToys.jsx
+++ src/pages/Toys/MyToys/MyToys.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext, useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
+import Swal from "sweetalert2";
 import { AuthContext } from "../../../providers/AuthProvider";
 import UpdateModal from "./UpdateModal";
 
@@ -31,6 +32,32 @@ const MyToys = () => {
       });
   };
 
+  const handleToyDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "I'm sure",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/postToys/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              toast.success("Toy deleted successfully");
+              const remaining = myToys?.filter((booking) => booking._id !== id);
+              setMyToys(remaining);
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="max-w-screen-xl mx-auto my-20 md:px-10 lg:px-0">
       <h3 className="text-2xl font-bold">My Add Toys</h3>
@@ -48,7 +75,7 @@ const MyToys = () => {
                           <img src={toys?.picture} alt="car picture" />
                         </figure>
                       </div>
-                      <div>
+                      <div className="text-left">
                         <h3 className="font-bold">{toys?.toy_name}</h3>
                         <p className="text-smt my-2">
                           Category: {toys?.sub_category?.value}
@@ -59,21 +86,26 @@ const MyToys = () => {
                       </div>
                     </div>
                   </td>
-                  <td>
+                  <td className="text-center">
                     <span>Price: ${toys?.price}</span>
                     <br />
                     <span className="badge badge-ghost badge-sm">
                       Quantity: {toys?.quantity}
                     </span>
                   </td>
-                  <td className="ms-auto">
+                  <td className="text-right">
                     <UpdateModal
                       toys={toys}
                       handleToyUpdate={handleToyUpdate}
                     />
                   </td>
-                  <td>
-                    <button className="btn btn-warning ml-10">Delete</button>
+                  <td className="text-right">
+                    <button
+                      onClick={() => handleToyDelete(toys?._id)}
+                      className="btn btn-warning ml-10"
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               </tbody>
